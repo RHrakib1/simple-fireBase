66538a74c64218309c02be862573616981b55d54
RHrakib1 <rakibhasanmd457@gmail.com>
2024-03-30 12:32:25 +0600

    firebaase config

---
+++ src/Firebase/Firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA9UuItfVaIAvyU_kp3qWeTCLOcDWTgTyo",
+  authDomain: "simple-project-4b8f7.firebaseapp.com",
+  projectId: "simple-project-4b8f7",
+  storageBucket: "simple-project-4b8f7.appspot.com",
+  messagingSenderId: "682599302076",
+  appId: "1:682599302076:web:bb93d9d71596670d49a1b9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/__step.js
+++ src/__step.js
@@ -4,4 +4,5 @@
  * 3.register app
  * 4.instaall firebase
  * 5.project er modhe config k add krbo
+ * 6.denger: firebaseConfig karo sathe shear kora jabe na 
  */
